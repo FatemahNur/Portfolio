576d9de04bece2706ee6602a0354d8e5f5b28b81
FATIMA NOOR <fsayedno@uwo.ca>
2024-10-19 13:50:42 -0400

    updated portfolio

---
+++ scipt.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files like CSS
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
